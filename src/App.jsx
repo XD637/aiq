9320da10434aa43cd16bc4b15a69cedf52eca69f
XD637 <rithveya@gmail.com>
2025-08-13 11:11:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,200 @@
+import "./App.css";
+import search_icon from "./assets/search_icon.png";
+import AIQ_logo from "./assets/AIQ_logo.png";
+import fff from "./assets/MintIcon.png";
+// import './index.css'
+import i_icon from './assets/i_icon.svg'
+
+
+function App() {
+  return (
+    <div className="h-screen">
+      <div className="flex justify-between items-center bg-[#131313] px-[120px] py-[14px] text-white font-[Haas_Grot_Disp_Trial]">
+        <div>
+          <img src={AIQ_logo} alt="AiQ logo" />
+        </div>
+
+        <div>
+          <a href="">products</a>
+          <a href="" className="px-[40px]">
+            Market
+          </a>
+          <a href="">$AIG</a>
+        </div>
+
+        <div className="flex items-center">
+          <button className="whitespace-nowrap mr-[32px] font-medium bg-black rounded-xl py-2 px-7">
+            connect wallet{" "}
+          </button>
+          <img src={search_icon} alt="" className="h-6 w-6" />
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-t from-[#101010] to-[#2A2A2A] h-screen ">
+        <div className="text-white pt-[76px] mb-[45px] ">
+          <h1 className="mb-[16px] text-center ">Stake More, Earn More</h1>
+          <p className="text-[#F9F9F999]  text-center  font-gasp">
+            Select from Silver, Golden, or Platinum plans and maximize your{" "}
+            <br /> returns with flexible durations.
+          </p>
+        </div>
+
+        <div className="flex   whitespace-nowrap  ">
+          <div className="bg-[#141414] text-white rounded-2xl pt-[18px] max-w-[344px] w-full  ">
+            <figure className="mb-[32px] flex justify-center flex-col items-center ">
+              <div className="bg-[#1E1E1E] rounded-full m-auto    w-[88px] h-[88px] flex items-center justify-center ">
+                <img src={fff} alt="" className="  p-[17px] m-auto " />
+              </div>
+              <figcaption className="font-medium text-2xl mt-2">
+                Minting
+              </figcaption>
+            </figure>
+
+            <div className="flex justify-between pl-[33px] pr-[49px] ">
+              <span className="">USDT Balance:</span>
+              <p>100</p>
+            </div>
+
+            <div className=" bg-[#191919]  mx-[18px] mt-[12px] rounded-[5px]  pl-[14px] pr-[26px]">
+              <div className="flex justify-between gap-0.5 border-b border-b-[#FFFFFF40]  py-[9px] ">
+                <span className="flex justify-center items-center ">APO <img src={i_icon} alt="i icon " className="w-[12px] h-3 ml-1" /></span>
+                <p>01%</p>
+              </div>
+
+              <div className="flex  justify-between border-b border-b-[#FFFFFF40]  py-[9px] ">
+                <span className="">Ends on</span>
+                <p>20 Aug 2026</p>
+              </div>
+
+              <div className="flex  justify-between   py-[8px] ">
+                <span className="">Daily rewards</span>
+                <p>1,254.8 1 INCH</p>
+              </div>
+            </div>
+
+            <div
+              className="mt-[32px] flex justify-center pb-1 mb-[21px]  "
+              style={{ zIndex: 29 }}
+            >
+              <button
+                className="whitespace-nowrap border one h-[41px] w-[237px]  bg-black mx-auto rounded-xl font-medium  relative  text-[16px] "
+                style={{
+                  background:
+                    "radial-gradient(circle at 85% -80%, rgba(255,255,255,0.3), transparent 30%)",
+                }}
+              >
+                Mint
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+          <div className="flex   whitespace-nowrap  ">
+          <div className="bg-[#141414] text-white rounded-2xl pt-[18px] max-w-[344px] w-full  ">
+            <figure className="mb-[32px] flex justify-center flex-col items-center ">
+              <div className="bg-[#1E1E1E] rounded-full m-auto    w-[88px] h-[88px] flex items-center justify-center ">
+                <img src={fff} alt="" className="  p-[17px] m-auto " />
+              </div>
+              <figcaption className="font-medium text-2xl mt-2">
+                Staking
+              </figcaption>
+            </figure>
+
+            <div className="flex justify-between pl-[33px] pr-[49px] ">
+              <span className="">USDT Balance:</span>
+              <p>100</p>
+            </div>
+
+            <div className=" bg-[#191919]  mx-[18px] mt-[12px] rounded-[5px]  pl-[14px] pr-[26px]">
+              <div className="flex justify-between gap-0.5 border-b border-b-[#FFFFFF40]  py-[9px] ">
+                <span className="flex justify-center items-center ">APO <img src={i_icon} alt="i icon " className="w-[12px] h-3 ml-1" /></span>
+                <p>01%</p>
+              </div>
+
+              <div className="flex  justify-between border-b border-b-[#FFFFFF40]  py-[9px] ">
+                <span className="">Ends on</span>
+                <p>20 Aug 2026</p>
+              </div>
+
+              <div className="flex  justify-between   py-[8px] ">
+                <span className="">Daily rewards</span>
+                <p>1,254.8 1 INCH</p>
+              </div>
+            </div>
+
+            <div
+              className="mt-[32px] flex justify-center pb-1 mb-[21px]  "
+              style={{ zIndex: 29 }}
+            >
+              <button
+                className="whitespace-nowrap border one h-[41px] w-[237px]  bg-black mx-auto rounded-xl font-medium  relative  text-[16px] "
+                style={{
+                  background:
+                    "radial-gradient(circle at 85% -80%, rgba(255,255,255,0.3), transparent 30%)",
+                }}
+              >
+                Mint
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+          <div className="flex   whitespace-nowrap  ">
+          <div className="bg-[#141414] text-white rounded-2xl pt-[18px] max-w-[344px] w-full  ">
+            <figure className="mb-[32px] flex justify-center flex-col items-center ">
+              <div className="bg-[#1E1E1E] rounded-full m-auto    w-[88px] h-[88px] flex items-center justify-center ">
+                <img src={fff} alt="" className="  p-[17px] m-auto " />
+              </div>
+              <figcaption className="font-medium text-2xl mt-2">
+                Minting
+              </figcaption>
+            </figure>
+
+            <div className="flex justify-between pl-[33px] pr-[49px] ">
+              <span className="">USDT Balance:</span>
+              <p>100</p>
+            </div>
+
+            <div className=" bg-[#191919]  mx-[18px] mt-[12px] rounded-[5px]  pl-[14px] pr-[26px]">
+              <div className="flex justify-between gap-0.5 border-b border-b-[#FFFFFF40]  py-[9px] ">
+                <span className="flex justify-center items-center ">APO <img src={i_icon} alt="i icon " className="w-[12px] h-3 ml-1" /></span>
+                <p>01%</p>
+              </div>
+
+              <div className="flex  justify-between border-b border-b-[#FFFFFF40]  py-[9px] ">
+                <span className="">Ends on</span>
+                <p>20 Aug 2026</p>
+              </div>
+
+              <div className="flex  justify-between   py-[8px] ">
+                <span className="">Daily rewards</span>
+                <p>1,254.8 1 INCH</p>
+              </div>
+            </div>
+
+            <div
+              className="mt-[32px] flex justify-center pb-1 mb-[21px]  "
+              style={{ zIndex: 29 }}
+            >
+              <button
+                className="whitespace-nowrap border one h-[41px] w-[237px]  bg-black mx-auto rounded-xl font-medium  relative  text-[16px] "
+                style={{
+                  background:
+                    "radial-gradient(circle at 85% -80%, rgba(255,255,255,0.3), transparent 30%)",
+                }}
+              >
+                Mint
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
